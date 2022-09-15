4eadbb4de5ad2f278b214187d7c678312538c09c
Thanh Nguyen <nvthanh1804@gmail.com>
2022-09-15 21:51:57 +0900

    fix git ignore

--- components/Home/MealHistory.js
+++ components/Home/MealHistory.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const MealHistory = ({ image, date, type }) => {
   return (
-    <div className='relative h-[234px]'>
+    <div className='relative h-[234px] cursor-pointer'>
       <Image src={image} layout='fill' priority alt={type} objectFit='cover' />
       <span className='absolute bottom-0 px-2 py-1 text-white text-sm bg-ha-primary-300'>
         {moment(date, 'YYYY-MM-DD').format('MM.DD')}
